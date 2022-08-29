33998b06d045c0dba58c9c299bf64fab4a278177
Anas <anasbinateeq15@gmail.com>
2022-08-29 08:37:24 +0500

    class based components part 2 (like Button)

--- src/blogCard.js
+++ src/blogCard.js
@@ -1,17 +1,32 @@
-import React from "react"
+import React, { Component } from "react"
 import { dumplogs } from "./util";
 import classes from './BlogCard.module.css'
 
-const BlogCard = (props) => {
+class BlogCard extends Component{
 
-   dumplogs(props);
+   state = {
+    likeCount : 0
+   }
 
-    return(
-      <div className={classes.blogCard}>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-      </div>
-    )
+   onLikeBtnClick = () => {
+    this.setState((prevState, prevProp) =>{
+      return {likeCount : prevState.likeCount+1}
+    });
+   }
+
+    render(){
+
+      dumplogs(this.props);
+
+      return(
+        <div className={classes.blogCard}>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.description}</p>
+          <p>Like Count: <span className={classes.likeCount}>{this.state.likeCount}</span></p>
+          <button onClick={this.onLikeBtnClick}>Like</button>
+        </div>
+      )
+    }
   }
 
 export default BlogCard;
