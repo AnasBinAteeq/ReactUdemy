fb1d01e3223468850a598b1e4d8825b6a7089d1d
Anas <anasbinateeq15@gmail.com>
2022-08-29 07:48:11 +0500

    css modules

--- src/blogCard.js
+++ src/blogCard.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { dumplogs } from "./util";
+import classes from './BlogCard.module.css'
 
 const BlogCard = (props) => {
 
    dumplogs(props);
 
     return(
-      <div className='blogCard'>
+      <div className={classes.blogCard}>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
       </div>
